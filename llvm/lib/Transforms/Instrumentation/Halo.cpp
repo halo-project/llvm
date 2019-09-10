aedc5647a9042a0037e8513e02a5418ce85be403
Kavon Farvardin <kavon@farvard.in>
2019-09-10 16:28:06 -0500

    [halo] working on halo-prepare pass

--- llvm/lib/Transforms/Instrumentation/Halo.cpp
+++ llvm/lib/Transforms/Instrumentation/Halo.cpp
@@ -12,7 +12,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Instrumentation/Halo.h"
-#include "llvm/Analysis/CallGraph.h"
+// #include "llvm/Analysis/CallGraph.h"
+#include "llvm/IR/GlobalValue.h"
 
 #define DEBUG_TYPE "halo-prepare"
 
@@ -20,7 +21,34 @@ using namespace llvm;
 
 struct HaloPrepare {
   PreservedAnalyses runOnModule(Module &M) {
-    return PreservedAnalyses::all();
+
+    // We need to expose mutable globals defined in this module to the
+    // dynamic linker by marking them as external in this module.
+    //
+    // NOTE: we may need to do this via creating freshly-named aliases
+    // that are global, and perform a renaming during JIT compilation to
+    // prevent a name clash.
+    for (GlobalVariable &Global : M.globals()) {
+      if (Global.isDeclaration() || Global.isConstant())
+        continue;
+
+      if (Global.getName().startswith("llvm."))
+        continue;
+
+      LLVM_DEBUG(dbgs() << "before: \n\t" << Global << "\n");
+
+      Global.setLinkage(GlobalValue::ExternalLinkage);
+      Global.setDSOLocal(false);
+
+      LLVM_DEBUG(dbgs() << "after: \n\t" << Global << "\n\n");
+    }
+
+    // TODO: embed information about the calling convention used by
+    // each function in this module. We might be saved by the fact that
+    // all XRay-instrumented functions might end up using the default C
+    // calling convention?
+
+    return PreservedAnalyses::none(); // conservative guess for now
   }
 };
 
