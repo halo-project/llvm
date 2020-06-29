ca6e7c07e1bc2aacbf8d37491f65b25a8c2bc265
Kavon Farvardin <kavon@farvard.in>
2020-06-28 19:32:23 -0500

    add LoopPredication between LICM and LoopUnswitch in the new PM pipeline.
    I want to tune the choice of using its profitability check or applying it blindly

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -482,6 +482,7 @@ FunctionPassManager PassBuilder::buildO1FunctionSimplificationPipeline(
   LPM1.addPass(LoopRotatePass(/* Disable header duplication */ true));
   // TODO: Investigate promotion cap for O1.
   LPM1.addPass(LICMPass(PTO.LicmMssaOptCap, PTO.LicmMssaNoAccForPromotionCap));
+  LPM1.addPass(LoopPredicationPass());
   LPM1.addPass(SimpleLoopUnswitchPass());
   LPM2.addPass(IndVarSimplifyPass());
   LPM2.addPass(LoopIdiomRecognizePass());
